5087ff56e68d79aa6a6261662ce4b364db4d0bcf
Amita <amita.parikh@gmail.com>
2019-04-28 19:53:33 +0000

    Added in CSS stylings

--- frontend/src/index/Index.js
+++ frontend/src/index/Index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import './Index.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCube, faUserCheck, faListUl, faSearch } from '@fortawesome/free-solid-svg-icons'
+
 //Version of the sidebar shown before  logging in
 import PreSideBar from './preLoginSidebar';
 //Version of the sidebar shown after loggin in
@@ -68,6 +71,39 @@ class Home extends Component {
         <PreSideBar />
         <div>
           <h2 className="home-page">Pharma Rex</h2>
+          <h2 className="secondary-line">The best way to track your medications. </h2>
+
+          <ul className="pitch">
+          <li className="pitch__bullet">
+            <FontAwesomeIcon icon={faUserCheck} className="pitch__icon" />
+              <p>Free to sign up and use - no credit card needed!</p>
+            </li>
+            <li className="pitch__bullet">
+              <FontAwesomeIcon icon={faSearch} className="pitch__icon" />
+              <p>Manage all your medications from one centralized page</p>
+            </li>
+            <li className="pitch__bullet">
+              <FontAwesomeIcon icon={faListUl} className="pitch__icon" />
+              <p>Caregivers can monitor loved ones from afar.</p>
+            </li>
+        </ul>
+
+
+
+          <div className="spacer5" />
+          <div className="vertical-buttonpanel">
+            <Link className="buttontransparent home" to="/Login">Login</Link>
+            <Link className="buttonprimary" to="/Register">Register</Link>
+          </div>
+
+
+          <div className="links-to-info">
+            <p>
+              View Pharma Rex on <a className="project_link" href="https://github.com/LaviIonas/Pharma-Rex">Github</a>.</p>
+            <p>
+            Made using what we learned at <a className="project__link" href="https://lighthouselabs.com/">Lighthouse Labs</a>.
+            </p>
+          </div>
 
           <button onClick={this.sendText}>Send Text</button>
 
@@ -100,7 +136,7 @@ class Profile extends Component {
       <div>
         <PostSideBar />
         <div>
-          <h2 className="home-page">Register A New Profile</h2>
+          <h2 className="home-page">Create A New Profile</h2>
         </div>
       </div>
 
@@ -113,9 +149,23 @@ function About () {
     <div>
       <PostSideBar />
       <div>
-        <h2 className="home-page">Register A New Profile</h2>
-      </div>
-    </div>
+        <h2 className="home-page">About Pharma Rex</h2>
+          <div className="about-text">
+            <p>Here is where some text goes about the actual product</p>
+              <button className="animate-button">Remember to take
+                <span>your pills!.</span>
+              </button>
+              <h2>What is Pharma Rex?</h2>
+      <p>Pharma Rex is an easy to use tool that helps patients and their caregivers manage their medications. It's free to use and fun to interact with. Never miss a pill again!
+      </p>
+
+        <br />
+      <h2>Why did you make Pharma Rex?</h2>
+      <p>Pharma Rex started like so many other products: we wanted to solve a problem. In the United States, patients failing to take their prescribed medications cost the healthcare system
+      over $300 billion last year. The problem isn't only due to the cost of medications, either. In the UK, which offers subsized or free medications, 1 in 5 patients admits to forgetting to take their medication. We wanted to fix that.</p>
+            </div>
+          </div>
+        </div>
 
   );
 }
@@ -125,7 +175,7 @@ function Contact () {
     <div>
       <PostSideBar />
       <div>
-        <h2 className="home-page">Register A New Profile</h2>
+        <h2 className="home-page">Get in touch with us</h2>
       </div>
     </div>
 
@@ -137,7 +187,7 @@ function Settings () {
     <div>
       <PostSideBar />
       <div>
-        <h2 className="home-page">Register A New Profile</h2>
+        <h2 className="home-page">Edit your profile</h2>
       </div>
     </div>
 
