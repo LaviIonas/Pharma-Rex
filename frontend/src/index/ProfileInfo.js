e4d8bc5350b8496ed3ed4b03662be0d536e1774c
LaviIonas <laviionas772@gmail.com>
2019-04-28 21:00:00 +0000

    Creates the profile pic and info from express server

---
+++ frontend/src/index/ProfileInfo.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import './Index.css';
+import axios from 'axios';
+import profilePic from '../logo.svg';
+axios.defaults.baseURL = 'http://localhost:3001'
+
+
+
+class ProfileInfo extends Component {
+  constructor () {
+    super();
+    this.state = {
+      name: "",
+      careID: ""
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get("/profile/data/request")
+    .then((res) => {
+      console.log(res);
+      this.setState ({
+        name: res.data.name,
+        careID: res.data.careID
+      })
+    })
+  }
+  render () {
+    return (
+      <div>
+        <img src={profilePic} className = "profile-pic" alt="Logo" />
+        <p>Name: {this.state.name}</p>
+        <p>ID: {this.state.careID}</p>
+
+      </div>
+    );
+  }
+}
+
+export default ProfileInfo;
