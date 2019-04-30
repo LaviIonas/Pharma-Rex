0a84fd1d997a48cc4462280007c3a52d02f92c93
LaviIonas <laviionas772@gmail.com>
2019-04-30 21:16:55 +0000

    modified Route to manage axios requests from profile page

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -3,34 +3,28 @@
 const express = require('express');
 const router  = express.Router();
 
-const obj = {
-  name: "Ron Swanson",
-  careID: "123123432"
-}
-
-const data = {}
 
 module.exports = () => {
 
+  //Route Recieves Drug data when user creates a new one
   router.post("/data/new-drug", (req, res) => {
-    console.log("name:", req.body.name);
-    console.log("dose:", req.body.dose);
-    console.log("interval:", req.body.interval);
-    data.name = req.body.name;
-    data.dose = req.body.dose;
-    data.interval = req.body.interval;
+    const {name, dose, total, interval, time} = req.body;
 
-    console.log("New Drug Data Submitted");
+
     res.status(200).end();
   });
 
-  router.get("/data/new-drug", (req,res) => {
-    res.json(data);
-  });
+  //Empty Route sending an ARRAY of objects of drugs
+  //This route is responcible for filling the state array of existing added drugs
+  router.get("data/fill-array", (req,res) => {
+    //Create an array of objects of the drug info
+    //Example: [{name: "Heroin", dose: "2"}, {name: "Cocain", dose: "1"}]
 
-  router.get("/data/request", (req, res) => {
-    res.json(obj);
-    console.log("Data sent to profile page");
+  })
+
+  //Empty Route for Profile info (requested varibales are accepted (res.data.variable) as mentioned)
+  router.get("/data/profileInfo", (req, res) => {
+    //Send me data from the DB please (name, careID, doctor, pharmacyNum)
   });
 
   return router;
