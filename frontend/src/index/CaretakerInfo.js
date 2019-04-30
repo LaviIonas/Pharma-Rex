786f029a7ac7a463a882ebb67deaf447181e31b2
LaviIonas <laviionas772@gmail.com>
2019-04-30 21:17:53 +0000

    Created a new file to handle displaying caretaker info

---
+++ frontend/src/index/CaretakerInfo.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import './Index.css';
+import axios from 'axios';
+import profilePic from '../care.svg';
+axios.defaults.baseURL = 'http://localhost:3001'
+
+
+class CaretakerInfo extends Component {
+  constructor () {
+    super();
+    this.state = {
+      name: "",
+      patientArray: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get("/caretaker/data/caretakerInfo")
+    .then((res) => {
+      console.log(res);
+      this.setState ({ name: res.data.name })
+      res.data.array.forEach((patient) => {
+        this.setState({
+        //Seed the array
+        patientArray: [...this.state.patientArray, patient]
+        })
+      })
+
+    })
+  }
+  render () {
+    return (
+      <div>
+        <img src={profilePic} className = "profile-pic" alt="Logo" />
+        <p>Name: {this.state.name}</p>
+        this.state.PatientArray.map(patient => {
+            return <Patients name={patient.name} />
+
+          })
+        }
+      </div>
+    );
+  }
+}
+
+class Patients extends Component {
+  render() {
+    return (
+      <div >
+        //-----------//
+
+        <p>{this.props.name}</p>
+
+      </div>
+      );
+  }
+}
+
+export default CaretakerInfo;
