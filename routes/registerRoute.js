b15d374faf84fd69a47486ac69fbe64d86590387
Mesel Isaac <meselisaac03@hotmail.com>
2019-05-01 12:24:24 -0400

    cookies set when rigistering as new patient, still issue with new caregiver

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -5,33 +5,68 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+  // router.post("/", (req, res) => {
+  //   //replace "req.body.phone" with "patient"
+  //   if (req.body.phone) {
+  //     knex('patients').insert({ email: req.body.username, password: req.body.password }).returning('id')
+  //     .asCallback(function (err, rows) {
+  //       if (err) {
+  //         res.status(500).end()
+  //           return
+  //       }
+  //       req.session.user_id = rows[0]
+  //       res.status(200).end();
+  //     })
+  //     //replace "req.body.age" with "caregiver"
+  //   } else if (req.body.age) {
+  //     knex('caregivers').insert({ email: req.body.username, password: req.body.password }).returning('id')
+  //     .asCallback(function (err, rows) {
+  //       if (err) {
+  //         res.status(500).end()
+  //         return
+  //       }
+  //       req.session.user_id = rows[0]
+  //       res.status(200).end();
+  //     })
+
+  //   }
+
+  // });
+
   router.post("/", (req, res) => {
-    //replace "req.body.phone" with "patient"
-    if (req.body.phone) {
+    console.log("REQUBODY", req.body)
+  
+    if (req.body.status === 'Patient') {
       knex('patients').insert({ email: req.body.username, password: req.body.password }).returning('id')
-      .asCallback(function (err, rows) {
-        if (err) {
-          res.status(500).end()
-            return
-        }
-        req.session.user_id = rows[0]
-        res.status(200).end();
-      })
-      //replace "req.body.age" with "caregiver"
-    } else if (req.body.age) {
-      knex('caregivers').insert({ email: req.body.username, password: req.body.password }).returning('id')
-      .asCallback(function (err, rows) {
+      .asCallback((err, rows) => {
         if (err) {
           res.status(500).end()
           return
         }
-        req.session.user_id = rows[0]
+        req.session.patient_id = rows[0]
+        console.log("REGISTER PATIENT_ID", req.session.patient_id)
+        res.status(200).end();
+      })
+   
+    } else if (req.body.status === 'Caregiver') {
+      knex('caregivers').insert({ email: req.body.username, password: req.body.password }).returning('id')
+      .asCallback((err, rows) => {
+        console.log ("ROWS", rows)
+        if (err) {
+          res.status(500).end()
+          return
+        }
+        
+        req.session.caregiver_id = rows[0]
+        console.log("REGISTER CAREGIVER_ID", req.session.caregiver_id)
         res.status(200).end();
       })
 
     }
 
   });
+
+
 
   return router;
 }
