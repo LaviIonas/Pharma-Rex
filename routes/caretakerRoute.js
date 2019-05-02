8f6553119389ea47c0b9598f1825ee93bd370e3b
Mesel Isaac <meselisaac03@hotmail.com>
2019-05-01 21:16:09 -0400

    caretaker route sends array of objects containing info on patient and medication they are taking to frontend

--- routes/caretakerRoute.js
+++ routes/caretakerRoute.js
@@ -9,34 +9,60 @@ module.exports = (knex) => {
   //Empty Route that takes care of feeding information to the
   //Caretaker page as well as feeding the array of patients
   router.get("/data/caretakerInfo", (req,res) => {
-    knex('patients').select('name').where({caregiver_id: req.session.caregiver_id})
+
+    knex.table('prescriptions').innerJoin('patients', 'prescriptions.patient_id', '=', 'patients.id').innerJoin('medications', 'medications.id', '=', 'prescriptions.medication_id').where({caregiver_id: req.session.caregiver_id})
     .then(rows => {
-      
       const array = []
+      rows.forEach(function (patientInfo){
+        let patientObj = {}
 
-      rows.forEach(function (name){
-        let obj = {}
-      
-        obj.name = name.name
-
-        array.push(obj)
-       
+        patientObj.name = patientInfo.name
+        patientObj.drug = patientInfo.medication_name
+        patientObj.dose = patientInfo.dosage
+        patientObj.pillsRemaining = patientInfo.total_number_pills
+        patientObj.time = patientInfo.start_time
+        patientObj.doctorName = patientInfo.doctor_name
+        patientObj.pharmacyNumber = patientInfo.pharmacy_number
+        patientObj.rxNumber = patientInfo.rx_number
         
 
+        array.push(patientObj)
 
-      })
+    })
+    res.json({array: array})
+
+    
+
+  })
+
+  //   knex('patients').select('name').where({caregiver_id: req.session.caregiver_id})
+  //   .then(rows => {
+      
+  //     const array = []
+
+  //     rows.forEach(function (name){
+  //       let obj = {}
+      
+  //       obj.name = name.name
+
+  //       array.push(obj)
+       
+        
+  //       // select * from prescriptions inner join patients on prescriptions.patient_id = patients.id inner join medications on medications.id = prescriptions.medication_id;
+
+  //     })
 
    
-      res.json({array: array})
+  //     res.json({array: array})
    
 
     
-    //respond with the name and array of patients
-    //Example: {name: "Bob", array: [{name: "Joe"}, {name: "Candy"}]}
+  //   //respond with the name and array of patients
+  //   //Example: {name: "Bob", array: [{name: "Joe"}, {name: "Candy"}]}
 
-  })
+  // })
   
-  
+// })
   
 })
 
