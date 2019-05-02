08cd5d63eab7a02e2bcb997fd5835e5c3500e230
Amita <amita.parikh@gmail.com>
2019-05-02 18:54:25 -0400

    Added in patient info hardcoded to /caregiver page

--- frontend/src/index/CaretakerInfo.js
+++ frontend/src/index/CaretakerInfo.js
@@ -10,26 +10,33 @@ class CaretakerInfo extends Component {
     super();
     this.state = {
       name: "Caregiver",
-      patientArray: []
+      patientArray: [{name: "Jaclyn",
+      drug: "Xanax",
+      dose: "2 a day",
+      pillsRemaining: "20",
+      time: "May 2nd, 2019",
+      doctorName: "Dr. Nick",
+      pharmacyNumber: "416-777-7777",
+      rxNumber: "RX12345"
+     }]
     }
   }
 
   componentDidMount() {
     axios
-    .get("/caretaker/data/caretakerInfo", {withCredentials: true})
-    .then((res) => {
-      console.log("NAME OR ARRAY? -------->", res);
-      this.setState ({ name: res.data.name })
-      let self = this;
-      res.data.array.forEach((patient) => {
-        self.setState({
-        //Seed the array
-        patientArray: [...this.state.patientArray, patient]
+      .get("/caretaker/data/caretakerInfo", {withCredentials: true})
+      .then((res) => {
+        console.log("NAME OR ARRAY? -------->", res);
+        this.setState ({ name: res.data.name })
+        let self = this;
+        res.data.array.forEach((patient) => {
+          self.setState({
+          //Seed the array
+            patientArray: [...this.state.patientArray, patient]
+          })
         })
+        console.log(this.state.patientArray);
       })
-      console.log(this.state.patientArray);
-
-    })
   }
 
   render () {
@@ -39,15 +46,15 @@ class CaretakerInfo extends Component {
         <img src={profilePic} className = "profile-pic" alt="Logo" />
         <p> Name: {this.state.name} </p>
         {
-        this.state.patientArray.map(patient => <Patient name={patient.name}
-                                                        drug={patient.drug}
-                                                        dose={patient.dose}
-                                                        pillsR={patient.pillsRemaining}
-                                                        time={patient.time}
-                                                        doctor={patient.doctorName}
-                                                        pharmacyN={patient.pharmacyNumber}
-                                                        rx={patient.rxNumber}
-                                                        />)
+          this.state.patientArray.map(patient => <Patient name={patient.name}
+            drug={patient.drug}
+            dose={patient.dose}
+            pillsR={patient.pillsRemaining}
+            time={patient.time}
+            doctor={patient.doctorName}
+            pharmacyN={patient.pharmacyNumber}
+            rx={patient.rxNumber}
+          />)
         }
 
 
@@ -64,17 +71,17 @@ class Patient extends Component {
       <div className="Caregiverpatientliststyle" >
         //-----------//
 
-        <p>{this.props.name}</p>
-        <p>{this.props.drug}</p>
-        <p>{this.props.dose}</p>
-        <p>{this.props.pillsR}</p>
-        <p>{this.props.time}</p>
-        <p>{this.props.doctor}</p>
-        <p>{this.props.pharmacyN}</p>
-        <p>{this.props.rx}</p>
+        <p>Patient Name: {this.props.name}</p>
+        <p>Patient Medication: {this.props.drug}</p>
+        <p>Dose: {this.props.dose}</p>
+        <p>Pills Remaining: {this.props.pillsR}</p>
+        <p>Time to take medication: {this.props.time}</p>
+        <p>Patient's Doctor: {this.props.doctor}</p>
+        <p>Patient's Pharmacy Phone Number: {this.props.pharmacyN}</p>
+        <p>Patient's RX Number: {this.props.rx}</p>
 
       </div>
-      );
+    );
   }
 }
 
